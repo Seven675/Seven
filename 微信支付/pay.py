8a5242b594694e32341a92802dffb97e5852b7db
‘mqk’ <m18501999474@163.com>
2020-03-09 17:11:04 +0800

    init

--- 微信支付/pay.py
+++ 微信支付/pay.py
@@ -1,127 +1,114 @@
-import requests
+from common.paykeys import client_appid, client_secret, Mch_id, Mch_key
 import hashlib
-import xmltodict
-import time
-import random
-import string
-
-from PCCR.config import APPID, MCHID, KEY, NOTIFY_URL
-
-from django.http import HttpResponse
+import datetime
+import xml.etree.ElementTree as ET
 
 
-# 生成nonce_str
-def generate_randomStr():
-    return ''.join(random.sample(string.ascii_letters + string.digits, 32))
-
-
-# 生成签名
-def generate_sign(param):
-    stringA = ''
-
-    ks = sorted(param.keys())
-    # 参数排序
-    for k in ks:
-        stringA += k + "=" + str(param[k]) + "&"
-    # 拼接商户KEY
-    stringSignTemp = stringA + "key=" + KEY
-
-    # md5加密
-    hash_md5 = hashlib.md5(stringSignTemp.encode('utf8'))
-    sign = hash_md5.hexdigest().upper()
-
-    return sign
-
-
-# 发送xml请求
-def send_xml_request(url, **param):
-    # dict 2 xml
-    param = {'root': param}
-    xml = xmltodict.unparse(param)
-
-    response = requests.post(url, data=xml.encode('utf-8'), headers={'Content-Type': 'text/xml'})
-    # xml 2 dict
-    msg = response.text
-    xmlmsg = xmltodict.parse(msg)
-
-    return xmlmsg
-
-
-# 统一下单
-def generate_bill(order_number, fee, openid):
-    url = "https://api.mch.weixin.qq.com/pay/unifiedorder"
-    nonce_str = generate_randomStr()  # 订单中加nonce_str字段记录（回调判断使用）
-    order_number = generate_order_number()  # 支付单号，只能使用一次，不可重复支付
-
-    '''
-    order.out_trade_no = out_trade_no
-    order.nonce_str = nonce_str
-    order.save()
-    '''
-
-    # 1. 参数
-    param = {
-        "appid": APPID,
-        "mch_id": MCHID,  # 商户号
-        "nonce_str": nonce_str,  # 随机字符串
-        "body": 'TEST_pay',  # 支付说明
-        "order_number": order_number,  # 自己生成的订单号
-        "total_fee": fee,
-        "spbill_create_ip": '127.0.0.1',  # 发起统一下单的ip
-        "notify_url": NOTIFY_URL,
-        "trade_type": 'JSAPI',  # 小程序写JSAPI
+# 生成签名的函数
+def paysign(appid, body, mch_id, nonce_str, notify_url, openid, out_trade_no, spbill_create_ip, total_fee):
+    ret = {
+        "appid": appid,
+        "body": body,
+        "mch_id": mch_id,
+        "nonce_str": nonce_str,
+        "notify_url": notify_url,
         "openid": openid,
+        "out_trade_no": out_trade_no,
+        "spbill_create_ip": spbill_create_ip,
+        "total_fee": total_fee,
+        "trade_type": 'JSAPI'
     }
-    # 2. 统一下单签名
-    sign = generate_sign(param)
-    param["sign"] = sign  # 加入签名
-    # 3. 调用接口
-    xmlmsg = send_xml_request(url, param)
-    # 4. 获取prepay_id
-    if xmlmsg['xml']['return_code'] == 'SUCCESS':
-        if xmlmsg['xml']['result_code'] == 'SUCCESS':
-            prepay_id = xmlmsg['xml']['prepay_id']
-            # 时间戳
-            timeStamp = str(int(time.time()))
-            # 5. 根据文档，六个参数，否则app提示签名验证失败，https://pay.weixin.qq.com/wiki/doc/api/app/app.php?chapter=9_12
-            data = {
-                "appid": APPID,
-                "partnerid": MCHID,
-                "prepayid": prepay_id,
-                "package": "Sign=WXPay",
-                "noncestr": nonce_str,
-                "timestamp": timeStamp,
-            }  # 6. paySign签名
-            paySign = generate_sign(data)
-            data["paySign"] = paySign  # 加入签名
-            # 7. 传给前端的签名后的参数
-            return data
+
+    # 处理函数，对参数按照key=value的格式，并按照参数名ASCII字典序排序
+    stringA = '&'.join(["{0}={1}".format(k, ret.get(k)) for k in sorted(ret)])
+    stringSignTemp = '{0}&key={1}'.format(stringA, Mch_key)
+    sign = hashlib.md5(stringSignTemp.encode("utf-8")).hexdigest()
+    return sign.upper()
 
 
-def payback(request):
-    msg = request.body.decode('utf-8')
-    xmlmsg = xmltodict.parse(msg)
+# 生成随机字符串
+def getNonceStr():
+    import random
+    data = "123456789zxcvbnmasdfghjklqwertyuiopZXCVBNMASDFGHJKLQWERTYUIOP"
+    nonce_str = ''.join(random.sample(data, 30))
+    return nonce_str
 
-    return_code = xmlmsg['xml']['return_code']
 
-    if return_code == 'FAIL':
-        # 官方发出错误
-        return HttpResponse("""<xml><return_code><![CDATA[FAIL]]></return_code>
-                            <return_msg><![CDATA[Signature_Error]]></return_msg></xml>""",
-                            content_type='text/xml', status=200)
+# 生成商品订单号
+def getWxPayOrdrID():
+    payOrdrID = Order.objects.get().order_number
+    print(payOrdrID)
+    # date = datetime.datetime.now()
+    # # 根据当前系统时间来生成商品订单号。时间精确到微秒
+    # payOrdrID = date.strftime("%Y%m%d%H%M%S%f")
 
-    elif return_code == 'SUCCESS':
-        # 拿到这次支付的订单号
-        order_number = xmlmsg['xml']['order_number']
-        # order = Order.objects.get(out_trade_no=out_trade_no)
-        if xmlmsg['xml']['nonce_str'] != order.nonce_str:
-            # 随机字符串不一致
-            return HttpResponse("""<xml><return_code><![CDATA[FAIL]]></return_code>
-                                        <return_msg><![CDATA[Signature_Error]]></return_msg></xml>""",
-                                content_type='text/xml', status=200)
+    return payOrdrID
 
-        # 根据需要处理业务逻辑
 
-        return HttpResponse("""<xml><return_code><![CDATA[SUCCESS]]></return_code>
-                            <return_msg><![CDATA[OK]]></return_msg></xml>""",
-                            content_type='text/xml', status=200)
+# 获取全部参数信息，封装成xml
+def get_bodyData(openid, client_ip, price):
+    body = 'Mytest'  # 商品描述
+    notify_url = 'https://cms-xinlixue.bnuonline.com/'  # 支付成功的回调地址  可访问 不带参数
+    nonce_str = getNonceStr()  # 随机字符串
+    out_trade_no = getWxPayOrdrID()  # 商户订单号
+    total_fee = str(price)  # 订单价格 单位是 分
+
+    # 获取签名
+    sign = paysign(client_appid, body, Mch_id, nonce_str, notify_url, openid, out_trade_no, client_ip, total_fee)
+
+    bodyData = '<xml>'
+    bodyData += '<appid>' + client_appid + '</appid>'  # 小程序ID
+    bodyData += '<body>' + body + '</body>'  # 商品描述
+    bodyData += '<mch_id>' + Mch_id + '</mch_id>'  # 商户号
+    bodyData += '<nonce_str>' + nonce_str + '</nonce_str>'  # 随机字符串
+    bodyData += '<notify_url>' + notify_url + '</notify_url>'  # 支付成功的回调地址
+    bodyData += '<openid>' + openid + '</openid>'  # 用户标识
+    bodyData += '<out_trade_no>' + out_trade_no + '</out_trade_no>'  # 商户订单号
+    bodyData += '<spbill_create_ip>' + client_ip + '</spbill_create_ip>'  # 客户端终端IP
+    bodyData += '<total_fee>' + total_fee + '</total_fee>'  # 总金额 单位为分
+    bodyData += '<trade_type>JSAPI</trade_type>'  # 交易类型 小程序取值如下：JSAPI
+    bodyData += '<sign>' + sign + '</sign>'
+    bodyData += '</xml>'
+
+    return bodyData
+
+
+def xml_to_dict(xml_data):
+    '''
+    xml to dict
+    :param xml_data:
+    :return:
+    '''
+    xml_dict = {}
+    root = ET.fromstring(xml_data)
+    for child in root:
+        xml_dict[child.tag] = child.text
+    return xml_dict
+
+
+def dict_to_xml(dict_data):
+    '''
+    dict to xml
+    :param dict_data:
+    :return:
+    '''
+    xml = ["<xml>"]
+    for k, v in dict_data.iteritems():
+        xml.append("<{0}>{1}</{0}>".format(k, v))
+    xml.append("</xml>")
+    return "".join(xml)
+
+
+# 获取返回给小程序的paySign
+def get_paysign(prepay_id, timeStamp, nonceStr):
+    pay_data = {
+        'appId': client_appid,
+        'nonceStr': nonceStr,
+        'package': "prepay_id=" + prepay_id,
+        'signType': 'MD5',
+        'timeStamp': timeStamp
+    }
+    stringA = '&'.join(["{0}={1}".format(k, pay_data.get(k)) for k in sorted(pay_data)])
+    stringSignTemp = '{0}&key={1}'.format(stringA, Mch_key)
+    sign = hashlib.md5(stringSignTemp.encode("utf-8")).hexdigest()
+    return sign.upper()
